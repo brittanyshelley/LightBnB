a2ffcde85a668927bf40e5966b42fd5790118087
brittanyshelley <brittany.j.shelley@gmail.com>
2023-12-15 09:51:58 -0700

    add reservations query

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -94,9 +94,26 @@ const addUser = function (user) {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+const getAllReservations = function(guest_id, limit = 10) {
+  const queryString = `
+    SELECT reservations.*, properties.*, AVG(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
+    LIMIT $2;`;
+
+  const values = [guest_id, limit];
+
+  return pool.query(queryString, values)
+    .then(res => res.rows)
+    .catch(err => console.error('Error executing query', err.stack));
 };
+
+
 
 /// Properties
 
